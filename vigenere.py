013027a3c13e1d5ac632d8cfde4b9f17585b54af
dan-smithy <dan-smithy@hushmail.com>
2014-04-27 19:47:53 +0100

    Update vigenere.py

--- vigenere.py
+++ vigenere.py
@@ -1,46 +1,46 @@
 #!/usr/bin/env python3
 
-class Rows( object ):
+class Rows(object):
     """
        This class is just used to produce the 
        rows of letters required for a vigenere
        cipher
     """
     
-    def __init__( self ):
+    def __init__(self):
         self.rows = {}
         self.alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
         self.gen_rows()
     
     
-    def shift( self, char, key ):
+    def shift(self, char, key):
         """
            Essentially a ceaser cipher, which is 
            passed one character and a shift key at a time
         """
-        shifted = chr(ord( char ) + key)
+        shifted = chr(ord(char) + key)
         if shifted > 'Z':
-            return chr(ord( shifted ) - 26)
+            return chr(ord(shifted) - 26)
         else:
             return shifted
 
     
-    def gen_rows( self ):
+    def gen_rows(self):
         """
            This creates the key-value pairs and inserts
            them into the rows dict
         """
-        for i in range( 26 ):
+        for i in range(26):
             string = ''
             for char in self.alphabet:
-                string += self.shift( char, i )
-            self.rows[ self.alphabet[i] ] = string
+                string += self.shift(char, i)
+            self.rows[self.alphabet[i]] = string
             
 
 
-class Main( object ):
+class Main(object):
     
-    def __init__( self ):
+    def __init__(self):
         """
            Creates an instance of the Rows class. It also gets the 
            message and the cipher key, converts both to uppercase, 
@@ -50,11 +50,11 @@ class Main( object ):
         self.char_block = Rows()
         self.text = input("Enter the message: ").upper().replace(" ", "")
         users_key = input("Enter the key: ").upper()
-        self.key = (users_key * len( self.text ))[0:len( self.text )]
+        self.key = (users_key * len(self.text))[0:len(self.text)]
         self.choice()
         
         
-    def choice( self ):
+    def choice(self):
         """
            Simply asks user whether the message will be deciphered 
            or enciphered.
@@ -68,28 +68,28 @@ class Main( object ):
             print("Invalid option! Learn to type...")
         
     
-    def encipher( self ):
+    def encipher(self):
         """
            Loops through both the message and the key. Enciphers each
            character of the message using the key and adds to ciphertext 
            string.
         """
         ciphertext = ""
-        for pt, key_char in zip( self.text, self.key ):
-            char_index = self.char_block.alphabet.index( pt )
+        for pt, key_char in zip(self.text, self.key):
+            char_index = self.char_block.alphabet.index(pt)
             ciphertext += self.char_block.rows[key_char][char_index]
-        print( ciphertext )
+        print(ciphertext)
         
     
-    def decipher( self ):
+    def decipher(self):
         """
            Does the opposite of encipher.
         """
         plaintext = ""
-        for ct, key_char in zip( self.text, self.key ):
-            char_index = self.char_block.rows[key_char].index( ct )
+        for ct, key_char in zip(self.text, self.key):
+            char_index = self.char_block.rows[key_char].index(ct)
             plaintext += self.char_block.alphabet[char_index]
-        print( plaintext )
+        print(plaintext)
         
         
         
